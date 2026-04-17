1923e3f37ba36239c3fbde586603a177d6b56d66
Sachin Banjade <banjadesachin2060@gmail.com>
2026-04-17 12:50:09 -0500

    final amplify app

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
+import '@aws-amplify/ui-react/styles.css'
+
+Amplify.configure(outputs)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
